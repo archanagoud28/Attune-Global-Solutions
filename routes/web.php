ee5fdb67ed6145599314ee24605228b8b3d950ca
Asapu Sri Kumar Manikanta <manikanta.asapu@paygdigitals.com>
2023-11-22 18:58:37 +0530

    Changes

--- routes/web.php
+++ routes/web.php
@@ -36,32 +36,16 @@ Route::middleware(['checkAuth'])->group(function () {
     Route::get('/HrLogin', HrLogin::class)->name('hr-login');
 });
 Route::middleware(['auth:hr'])->group(function () {
-    Route::get('/', Home::class);
+    Route::get('/', HomePage::class);
     Route::get('/emp-register', EmpRegister::class)->name('emp-register');
     Route::get('/emp-account-details', EmpAccountDetails::class)->name('emp-account-details');
     Route::get('/emp-family-details', EmpFamilyDetails::class)->name('emp-family-details');
     Route::get('/employee-page', EmployeePage::class)->name('employee-page');
-    Route::get('/home-page', HomePage::class)->name('home-page');
     Route::get('/contractor-page', ContractorPage::class)->name('contractor-page');
     Route::get('/vendor-page', VendorPage::class)->name('vendor-page');
     Route::get('/vendor-register', VendorRegister::class)->name('vendor-register');
     Route::get('/customers', Customers::class);
-<<<<<<< HEAD
     Route::get('/sales-or-purchase-orders', SalesOrPurchaseOrders::class);
-    Route::get('/emp-register', EmpRegister::class)->name('emp-register');
-    Route::get('/emp-account-details', EmpAccountDetails::class)->name('emp-account-details');
-    Route::get('/emp-family-details', EmpFamilyDetails::class)->name('emp-family-details');
-    Route::get('/emp-register', EmpRegister::class)->name('emp-register');
-    Route::get('/emp-account-details', EmpAccountDetails::class)->name('emp-account-details');
-    Route::get('/emp-family-details', EmpFamilyDetails::class)->name('emp-family-details');
-    Route::get('/employee-page', EmployeePage::class)->name('employee-page');
-    Route::get('/', HomePage::class)->name('home-page');
-    Route::get('/emp-register', EmpRegister::class)->name('emp-register');
-    Route::get('/emp-account-details', EmpAccountDetails::class)->name('emp-account-details');
-    Route::get('/emp-family-details', EmpFamilyDetails::class)->name('emp-family-details');
-    Route::get('/employee-page', EmployeePage::class)->name('employee-page');
-=======
->>>>>>> 637595e6343f6f5693bcc524fe88f97206783e36
     Route::get('/time-sheet-display', TimeSheetDisplay::class)->name('time-sheet-display');
     Route::get('/purchase-order', PurchaseOrder::class)->name('purchase-order');
 });
